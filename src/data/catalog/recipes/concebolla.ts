585327c533208fd0fbeff9881cd8c2e8d598cc20
felix <felix@tortilladepatas.de>
2026-07-23 10:39:04 +0200

    corrected duplicated

--- src/data/catalog/recipes/concebolla.ts
+++ src/data/catalog/recipes/concebolla.ts
@@ -1,23 +1,24 @@
 /**
- * FILE: sincebolla.ts
+ * FILE: concebolla.ts
  *
  * PURPOSE:
- * Recipe definition for Tortilla sin cebolla.
+ * Recipe definition for Tortilla con cebolla.
  *
  * RESPONSIBILITY:
- * - Specifies ingredients needed for the sin cebolla recipe variant.
+ * - Specifies ingredients needed for the con cebolla recipe variant.
  */
 
-import { ingredients } from "../ingredients";
+import { ingredients } from '../ingredients'
 
 export const recipe = {
-  id: "sincebolla",
-  name: "Sincebolla",
+  id: 'concebolla',
+  name: 'Con Cebolla',
   ingredients: [
-    ingredients.find(i => i.id === "potato"),
-    ingredients.find(i => i.id === "egg"),
-    ingredients.find(i => i.id === "oil"),
-    ingredients.find(i => i.id === "salt"),
-    ingredients.find(i => i.id === "pepper"),
-  ]
-};
+    ingredients.find((i) => i.id === 'potato'),
+    ingredients.find((i) => i.id === 'egg'),
+    ingredients.find((i) => i.id === 'oil'),
+    ingredients.find((i) => i.id === 'salt'),
+    ingredients.find((i) => i.id === 'pepper'),
+    ingredients.find((i) => i.id === 'onion'),
+  ],
+}
